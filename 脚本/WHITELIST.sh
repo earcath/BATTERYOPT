82a73e740dc8e5f69de66c1f42b4e84bd97a7032
染崎枫 <102382352+SomesakiKaede@users.noreply.github.com>
2023-04-21 20:00:31 +0800

    Add files via upload

--- 脚本/WHITELIST.sh
+++ 脚本/WHITELIST.sh
@@ -1,5 +1,5 @@
 WHITELIST=/data/adb/BATTERYOPT/电池优化白名单.prop
 
 whitelist=$(cat "$WHITELIST" | grep -v '^#' | cut -f2 -d '=')
-whitelist=`pm list packages -e | sed "s/package:/-/g"`"$whitelist"
+whitelist=`pm list packages -3 | sed "s/package:/-/g"`"$whitelist"
 dumpsys deviceidle whitelist $whitelist
--- 脚本/service.sh
+++ 脚本/service.sh
@@ -1,5 +1,12 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
+WHITELIST=/data/adb/BATTERYOPT/电池优化白名单.prop
+
+if [ ! -f $WHITELIST ]; then
+	mkdir /data/adb/BATTERYOPT
+	touch $WHITELIST
+	echo "#加入包名，前面加＋号，如 +com.itisaapp.and" >$WHITELIST
+fi
 
 export PATH="/system/bin:/system/xbin:/vendor/bin:$(magisk --path)/.magisk/busybox:$PATH"
 crond -c $MODDIR/cron.d
